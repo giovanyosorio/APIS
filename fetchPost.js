e925d7ab8cfde57f60d5b26add21c60d073cbf40
giovany osorio <30839218+giovanyosorio@users.noreply.github.com>
2023-09-15 17:41:47 -0500

    Create fetchPost.js

---
+++ fetchPost.js
@@ -0,0 +1,27 @@
+const API= "https://api.escuelajs.co/api/v1/products"
+
+function postData(url,data){
+  const response=fetch(url,{
+    method:"POST",
+    mode:"cors",
+    credentials:"same-origin",
+    headers:{
+      "Content-Type":"application/json"
+    },
+    body:JSON.stringify(data)
+  })
+  return response
+}
+
+const data={
+  "title": "New Product",
+  "price": 9999,
+  "description": "A description",
+  "categoryId": 1,
+  "images": ["https://placeimg.com/640/480/any"]
+}
+
+postData(API,data)
+.then(response=>response.json())
+.then(data=>console.log(data))
+      
